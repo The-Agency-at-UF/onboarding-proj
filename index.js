10c63f2d089f77400599454cd504d5b7966eb378
Monica Tam <monicatam75@hotmail.com>
2021-11-07 16:58:03 -0500

    Simple setup created

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    console.log("Surpriseee Shawwwtyyy, make sure to use the console, it's a good way to check if a function is working.");
+}
+
+function surprise() {
+    var surprise = document.getElementById('surprise');
+    surprise.style.display = "initial";
+}
+
+function noSurprise() {
+    var surprise = document.getElementById('surprise');
+    surprise.style.display = "none";
+}
